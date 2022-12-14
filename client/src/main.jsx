f4c74bb66010d288ce13b7f50f55532f86003909
cheeter_Lee <johnson991210@gmail.com>
2022-12-14 19:46:09 +0800

    enable connecting to metamask and wrting to blockchain

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react'
-
+import { StateContextProvider } from './context'
 import App from './App'
 import './index.css'
 
@@ -11,7 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <ThirdwebProvider desiredChainId={ChainId.Goerli}>
         <BrowserRouter>
-            <App />
+            <StateContextProvider>
+                <App />
+            </StateContextProvider>
         </BrowserRouter>
     </ThirdwebProvider>
 )
--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { ethers } from 'ethers'
-
+import { useStateContext } from '../context'
 import { money } from '../assets'
 import { CustomButton, FormField } from '../components'
 import { checkIfImage } from '../utils'
 
 const CreateCampaign = () => {
+  const { createCampaign } = useStateContext
   const navigate = useNavigate()
   const [isLoading, setIsLoading] = useState(false)
   const [form, setForm] = useState({
@@ -22,10 +23,20 @@ const CreateCampaign = () => {
     setForm({ ...form, [fieldName]: e.target.value })
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    console.log(form)
 
+    checkIfImage(form.image, async (exists) => {
+      if (exists) {
+        setIsLoading(true)
+        await createCampaign({ ...form, target: ethers.utils.parseUnits(form.target, 18) })
+        setIsLoading(false)
+        navigate('/')
+      } else {
+        alert('Invalid image URL!')
+        setForm({ ...form, image: '' })
+      }
+    })
   }
 
 
